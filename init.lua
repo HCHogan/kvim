76ad0c1aedcc070d0fe904409bc03ecd64d2fd5a
hank <ysh2291939848@outlook.com>
2024-08-06 20:55:00 +0800

    udpate

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
-require 'custom.options'
-require 'custom.keymaps'
-require 'custom.autocommands'
+require 'core.options'
+require 'core.keymaps'
+require 'core.autocommands'
 
 local use_rocks = vim.fn.getenv 'KVIM_USE_ROCKS'
 
@@ -10,6 +10,3 @@ if use_rocks == '1' then
 else
   require 'lazy-nvim'
 end
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
