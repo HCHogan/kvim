beb1c239ef8ecf3c6cd2fd4f54b1e6af23e87364
hank <ysh2291939848@outlook.com>
2024-08-07 19:27:53 +0800

    update

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -35,13 +35,5 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- lspsafa
-vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', { desc = 'Hover symbol details' })
-vim.keymap.set('n', '<leader>lc', '<cmd>Lspsaga incoming_calls<CR>', { desc = 'Incoming calls' })
-vim.keymap.set('n', '<leader>lC', '<cmd>Lspsaga outgoing_calls<CR>', { desc = 'Outgoing calls' })
-vim.keymap.set('n', '<leader>a', '<cmd>Lspsaga code_action<CR>', { desc = 'LSP code action' })
-vim.keymap.set('x', '<leader>a', '<cmd>Lspsaga code_action<CR>', { desc = 'LSP code action' })
-vim.keymap.set('n', '<leader>lp', '<cmd>Lspsaga peek_definition<CR>', { desc = 'Peek definition' })
-vim.keymap.set('n', '<leader>lS', '<cmd>Lspsaga outline<CR>', { desc = 'Symbols outline' })
-vim.keymap.set('n', '<leader>lr', '<cmd>Lspsaga rename<CR>', { desc = 'Symbols rename' })
-vim.keymap.set('n', '<leader>lR', '<cmd>Lspsaga finder<CR>', { desc = 'Search references' })
+vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'Toggle comment line' })
+vim.keymap.set('v', '<leader>/', 'gc', { desc = 'Toggle comment line' })
