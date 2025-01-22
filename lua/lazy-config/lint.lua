fd3319d67b1ac886d30e9f1343ead8a77bbe4e36
Hank Hogan <ysh2291939848@outlook.com>
2025-01-22 11:33:15 +0000

    added nix formatter

--- lua/lazy-config/lint.lua
+++ lua/lazy-config/lint.lua
@@ -42,6 +42,7 @@ return {
         swift = { 'swiftformat' },
         typst = { 'typstyle' },
         markdown = { 'prettierd', 'prettier' },
+        nix = { 'alejandra'},
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
