d3acad5c0f7f09bd7a71f9455eaa3eaa792b100f
Hank C. Hogan <hank@flexus.openstacklocal>
2025-02-14 03:13:30 +0800

    go for 6.824

--- lua/lazy-config/lsp.lua
+++ lua/lazy-config/lsp.lua
@@ -143,6 +143,19 @@ return {
     lazy = false, -- This plugin is already lazy
   },
   {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
     'Julian/lean.nvim',
     event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
 
