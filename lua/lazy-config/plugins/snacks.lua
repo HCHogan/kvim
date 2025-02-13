653980e7fa2d7205413d77d46f9580df54c79f86
Hank C. Hogan <hank@flexus.openstacklocal>
2025-02-13 18:40:20 +0800

    use snacks.dashboard

---
+++ lua/lazy-config/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  opts = {
+    dashboard = {
+      enabled = true,
+    },
+    image = {
+      enabled = true,
+    }
+  },
+}
--- lua/lazy-config/ui.lua
+++ lua/lazy-config/ui.lua
@@ -45,45 +45,15 @@ return {
                 }, args.buf)
             return is_disabled
           end,
+          disable_statusline_cb = function(args)
+            local is_disabled = not require("heirline-components.buffer").is_valid(args.buf) or
+                lib.condition.buffer_matches({
+                  buftype = { "terminal", "prompt", "nofile", "help", "quickfix" },
+                  filetype = { "NvimTree", "neo%-tree", "dashboard", "Outline", "aerial", "avante" },
+                }, args.buf)
+            return is_disabled
+          end,
         },
-        tabline = { -- UI upper bar
-          lib.component.tabline_conditional_padding(),
-          lib.component.tabline_buffers(),
-          lib.component.fill { hl = { bg = "tabline_bg" } },
-          lib.component.tabline_tabpages()
-        },
-        winbar = { -- UI breadcrumbs bar
-          init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
-          fallthrough = false,
-          -- Winbar for terminal, neotree, and aerial.
-          {
-            condition = function() return not lib.condition.is_active() end,
-            {
-              lib.component.neotree(),
-              lib.component.compiler_play(),
-              lib.component.fill(),
-              lib.component.compiler_build_type(),
-              lib.component.compiler_redo(),
-              lib.component.aerial(),
-            },
-          },
-          -- Regular winbar
-          {
-            lib.component.neotree(),
-            lib.component.compiler_play(),
-            lib.component.fill(),
-            lib.component.breadcrumbs(),
-            lib.component.fill(),
-            lib.component.compiler_redo(),
-            lib.component.aerial(),
-          }
-        },
-        statuscolumn = { -- UI left column
-          init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
-          lib.component.foldcolumn(),
-          lib.component.numbercolumn(),
-          lib.component.signcolumn(),
-        } or nil,
         statusline = { -- UI statusbar
           hl = { fg = "fg", bg = "bg" },
           lib.component.mode(),
