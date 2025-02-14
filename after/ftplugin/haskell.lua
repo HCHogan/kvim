cee63e7279fd45bd7c7edda69a64d2e9e0cf052e
Hank C. Hogan <hank@flexus.openstacklocal>
2025-02-14 15:13:02 +0800

    hls eval keymap

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -7,9 +7,9 @@ vim.keymap.set('n', '<space>cl', vim.lsp.codelens.run, opts)
 -- Hoogle search for the type signature of the definition under the cursor
 vim.keymap.set('n', '<space>fg', ht.hoogle.hoogle_signature, opts)
 -- Evaluate all code snippets
-vim.keymap.set('n', '<space>ra', ht.lsp.buf_eval_all, opts)
+vim.keymap.set('n', '<space>le', ht.lsp.buf_eval_all, opts)
 -- Toggle a GHCi repl for the current package
-vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
+vim.keymap.set('n', '<leader>li', ht.repl.toggle, opts)
 -- Toggle a GHCi repl for the current buffer
 vim.keymap.set('n', '<leader>rf', function()
   ht.repl.toggle(vim.api.nvim_buf_get_name(0))
