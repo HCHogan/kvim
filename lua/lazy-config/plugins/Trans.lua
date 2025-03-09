f6724c73db41fab01820eaa21c5b948617071856
Hank C. Hogan <hank@flexus.openstacklocal>
2025-03-09 11:12:55 +0800

    updat

--- lua/lazy-config/plugins/Trans.lua
+++ lua/lazy-config/plugins/Trans.lua
@@ -1,7 +1,23 @@
 return {
   "JuanZoran/Trans.nvim",
+  dependencies = {
+    "kkharji/sqlite.lua",
+  },
   event = "VeryLazy",
+  keys = {
+    { "<Leader>lt", "<Cmd>Translate<CR>", mode = { "n", "x" }, desc = "Translate" }
+  },
   opts = {
-    theme = "catppuccin",
+    frontend = {
+      default = {
+        animation = {
+          open = false,
+          close = false,
+        }
+      },
+      hover = {
+        width = 42,
+      }
+    }
   }
 }
