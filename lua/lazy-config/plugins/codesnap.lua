6707c27e969d01a5b878b93e124a5e20092deaa9
Hank Hogan <ysh2291939848@outlook.com>
2025-03-20 16:34:02 +0800

    update

--- lua/lazy-config/plugins/codesnap.lua
+++ lua/lazy-config/plugins/codesnap.lua
@@ -2,4 +2,9 @@ return {
   "mistricky/codesnap.nvim",
   build = "make",
   event = "VeryLazy",
+  opts = {
+    bg_theme = "sea",
+    code_font_family = "Recursive",
+    has_line_number = true,
+  }
 }
