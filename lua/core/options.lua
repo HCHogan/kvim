fd7bc6cc6bf8bad3d527cb4afb7baa911f8025b2
Hank Hogan <ysh2291939848@outlook.com>
2025-05-21 22:55:33 +0800

    update

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -82,17 +82,30 @@ vim.diagnostic.config {
   severity_sort = true,
   float = { border = 'rounded', source = 'if_many' },
   underline = { severity = vim.diagnostic.severity.ERROR },
-  signs = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN]  = "",
+      [vim.diagnostic.severity.INFO]  = "",
+      [vim.diagnostic.severity.HINT]  = "",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+      [vim.diagnostic.severity.WARN]  = "DiagnosticWarn",
+      [vim.diagnostic.severity.INFO]  = "DiagnosticInfo",
+      [vim.diagnostic.severity.HINT]  = "DiagnosticHint",
+    },
+  },
 }
 
-for _, sev in ipairs({ "Error", "Warn", "Info", "Hint" }) do
-  local sign_name = "DiagnosticSign" .. sev
-  vim.fn.sign_define(sign_name, {
-    text   = "",
-    texthl = "",
-    numhl  = "Diagnostic" .. sev,
-  })
-end
+-- for _, sev in ipairs({ "Error", "Warn", "Info", "Hint" }) do
+--   local sign_name = "DiagnosticSign" .. sev
+--   vim.fn.sign_define(sign_name, {
+--     text   = "",
+--     texthl = "",
+--     numhl  = "Diagnostic" .. sev,
+--   })
+-- end
 
 if vim.g.neovide then
   local neovide_transparency = 0.75
