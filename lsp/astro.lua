82dfacab84d9ceaed62386e6d8eb18de662cfce7
Hank Hogan <ysh2291939848@outlook.com>
2025-05-29 13:44:42 +0800

    update

--- lsp/astro.lua
+++ lsp/astro.lua
@@ -14,4 +14,12 @@ return {
   init_options = {
     typescript = {},
   },
+  before_init = function(params, config)
+    if vim.tbl_get(config.init_options, "typescript") and not config.init_options.typescript.tsdk then
+      local project_root =
+          vim.fs.dirname(vim.fs.find("node_modules", { path = params.rootPath, upward = true })[1])
+      config.init_options.typescript.tsdk = project_root and (project_root .. "/node_modules/typescript/lib")
+          or ""
+    end
+  end,
 }
