e3dfbc7a5c10c1dc57aaea110640fdf637e03642
Hank Hogan <ysh2291939848@outlook.com>
2025-06-02 00:53:46 +0800

    udpate

--- lua/lazy-config/plugins/codesnap.lua
+++ lua/lazy-config/plugins/codesnap.lua
@@ -5,8 +5,8 @@ return {
   opts = {
     save_path = "~/Pictures",
     bg_theme = "bamboo",
-    bg_x_padding = 10,
-    bg_y_padding = 10,
+    bg_x_padding = 20,
+    bg_y_padding = 20,
     code_font_family = "Recursive",
     has_line_number = true,
     watermark = "",
