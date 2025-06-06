39d597fd30dd5a624e222a8a0993afcbaeadc05c
hank <ysh2291939848@outlook.com>
2025-06-06 14:32:24 +0800

    update

--- lua/lazy-config/cmp.lua
+++ lua/lazy-config/cmp.lua
@@ -90,12 +90,37 @@ return {
           winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
           draw = {
             treesitter = { "lsp" },
-            -- components = {
-            --   kind_icon = {
-            --     text = function(ctx) return get_kind_icon(ctx).text end,
-            --     highlight = function(ctx) return get_kind_icon(ctx).highlight end,
-            --   },
-            -- },
+            components = {
+              -- customize the drawing of kind icons
+              kind_icon = {
+                text = function(ctx)
+                  -- default kind icon
+                  local icon = ctx.kind_icon
+                  -- if LSP source, check for color derived from documentation
+                  if ctx.item.source_name == "LSP" then
+                    local color_item = require("nvim-highlight-colors").format(ctx.item.documentation,
+                      { kind = ctx.kind })
+                    if color_item and color_item.abbr ~= "" then
+                      icon = color_item.abbr
+                    end
+                  end
+                  return icon .. ctx.icon_gap
+                end,
+                highlight = function(ctx)
+                  -- default highlight group
+                  local highlight = "BlinkCmpKind" .. ctx.kind
+                  -- if LSP source, check for color derived from documentation
+                  if ctx.item.source_name == "LSP" then
+                    local color_item = require("nvim-highlight-colors").format(ctx.item.documentation,
+                      { kind = ctx.kind })
+                    if color_item and color_item.abbr_hl_group then
+                      highlight = color_item.abbr_hl_group
+                    end
+                  end
+                  return highlight
+                end,
+              },
+            },
           },
         },
         accept = {
