0fd3752cb66dffcd11adac85a8d47f95888d61b4
hank <ysh2291939848@outlook.com>
2025-06-07 18:36:22 +0800

    update

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -51,7 +51,8 @@ vim.lsp.config("*", {
   capabilities = capabilities
 })
 
-local lsps = { "clangd", "basedpyright", "luals", "bashls", "nil", "neocmake", "sourcekit", "yamlls", "tsls", "taplo", "astro", "html", "jsonls", "cssls" }
+local lsps = { "basedpyright", "luals", "bashls", "nil", "neocmake", "sourcekit", "yamlls", "tsls", "taplo", "astro",
+  "html", "jsonls", "cssls" }
 
 for _, lsp in ipairs(lsps) do
   if vim.fn.executable(vim.lsp.config[lsp].cmd[1]) == 1 then
--- lsp/sourcekit.lua
+++ lsp/sourcekit.lua
@@ -1,6 +1,6 @@
 return {
   cmd = { 'sourcekit-lsp' },
-  filetypes = { 'swift' },
+  filetypes = { 'c', 'cpp', 'objc', 'swift' },
   root_markers = { 'buildServer.json', '*.xcodeproj', '*.xcodeworkspace', 'compile_commands.json', 'Package.swift', '.git' },
   single_file_support = true,
   capabilities = {
