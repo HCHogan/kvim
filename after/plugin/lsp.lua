737b2f51bbf1c2350da301afcbca22c3263ded6e
Hank C. Hogan <hank@flexus.openstacklocal>
2025-06-08 14:04:34 +0800

    udpate

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -51,7 +51,7 @@ vim.lsp.config("*", {
   capabilities = capabilities
 })
 
-local lsps = { "basedpyright", "luals", "bashls", "nil", "neocmake", "sourcekit", "yamlls", "tsls", "taplo", "astro",
+local lsps = { "clangd", "basedpyright", "luals", "bashls", "nil", "neocmake", "sourcekit", "yamlls", "tsls", "taplo", "astro",
   "html", "jsonls", "cssls" }
 
 for _, lsp in ipairs(lsps) do
