133ece9ef704ce827a79a7a6e2f9f1c64bbbda6c
Hank C. Hogan <hank@flexus.openstacklocal>
2025-06-17 15:30:47 +0800

    update

---
+++ lua/lazy-config/plugins/nv.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvzone/floaterm",
+    dependencies = "nvzone/volt",
+    opts = {},
+    cmd = "FloatermToggle",
+  },
+  {
+    "nvzone/minty",
+    cmd = { "Shades", "Huefy" },
+  },
+  { "nvzone/volt", lazy = true },
+}
